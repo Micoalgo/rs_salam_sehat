571812e39aad7fc174c9585d6b569800e567a3a6
Micoalgo <123436963+Micoalgo@users.noreply.github.com>
2024-08-13 14:27:45 +0700

    Update rs_salam_sehat2.py

--- rs_salam_sehat2.py
+++ rs_salam_sehat2.py
@@ -139,10 +139,11 @@ with tabs[0]:
         with col11:
             st.subheader("Rangking Omset per Poli/Unit")
             st.image("bomset_poli.png")
-            st.write("Diagram batang ini menunjukkan rangking Poli dalam seluruh periodenya, mulai dari yang terbesar sampai dengan yg terkecil")
+            st.write("Diagram batang ini menunjukkan rangking Omset Poli dalam seluruh periodenya, mulai dari Poli dengan Omset terbesar sampai dengan Poli dengan omset terkecil")
         with col12:
             st.subheader("Komparasi Rangking Omset Poli/Unit per Periode")
             st.image("bomset_poli2.png")
+            st.write("Dengan Klik Pilihan Tahun pada sisi kanan atas, Diagram batang ini menunjukkan rangking dan Komparasi Omaset Poli pada masing-masing Tahunnya, mulai dari yang terbesar sampai dengan yg terkecil")
         with col11a:
             st.subheader("Rangking Jumlah Pasien per Poli/Unit")
             st.image("cpasien_poli.png")
