a51890ad455c00b32c4a862be6402e60ec9d9e70
Micoalgo <123436963+Micoalgo@users.noreply.github.com>
2024-08-13 13:36:02 +0700

    Update rs_salam_sehat2.py

--- rs_salam_sehat2.py
+++ rs_salam_sehat2.py
@@ -27,10 +27,9 @@ st.image("rs33b.png")
 st.markdown("--------")
 
 st.markdown('<p class="big-font">Kompleksitas Bisnis Rumah Sakit</p>', unsafe_allow_html=True)
-#st.subheader("*Apakah Owner dan Direktur RS Bingung dan Kesulitan melihat Progres Bisnis RS ??*")
 st.subheader(''':red[*Apakah Owner dan Direktur RS Bingung dan Kesulitan melihat Progres Bisnis RS ??*]''')
-st.subheader("*Apa yang harus dilakukan ?? Mulai dari mana ??*")
-st.subheader("*.....Sementara Biaya Rutin Operasional, Angsuran Alkes dan Biaya Lainnya harus terus keluar dan menggerus Cash flow RS*")
+st.subheader(''':red[*Apa yang harus dilakukan ?? Mulai dari mana ??*]")
+st.subheader(''':yellow-background[*.....Sementara Biaya Rutin Operasional, Angsuran Alkes dan Biaya Lainnya harus tetap keluar dan menggerus Cash flow RS*]''')
 
 
 with st.expander("Lingkup dan Penerapan Data Analis untuk Bisnis Rumah Sakit"):
