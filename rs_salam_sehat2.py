5a58bff9d18ad4704024dd1ba95abfc163c3f179
Micoalgo <123436963+Micoalgo@users.noreply.github.com>
2024-08-15 14:04:20 +0700

    Update rs_salam_sehat2.py

--- rs_salam_sehat2.py
+++ rs_salam_sehat2.py
@@ -415,6 +415,9 @@ with col200:
     st.markdown("[(Klik disini untuk Penjelasan Contoh Dashboard lainnya - Youtube video)](https://youtu.be/WHpYuC6DRhk?feature=shared)")
 
 st.markdown("--------")
+st.subheader(''':red[*Learn More...*]''')
+
+st.markdown("--------")
 st.write("*Created by Hospital Financial Analyst - HFA*")
 st.write("*Mico Chandra*")
 st.markdown("[(Klik Linkedin)](https://www.linkedin.com/in/mico-chandra-3a1708202/)")
