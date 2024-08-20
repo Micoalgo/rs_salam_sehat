7caa7c3fba9c84ead1706c25dfab9d6913c66179
Micoalgo <123436963+Micoalgo@users.noreply.github.com>
2024-08-20 11:40:17 +0700

    Update rs_salam_sehat2.py

--- rs_salam_sehat2.py
+++ rs_salam_sehat2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from streamlit_gsheets import GSheetsConnection
+#from streamlit_gsheets import GSheetsConnection
 
 #visualisasi
 import matplotlib.pyplot as plt
